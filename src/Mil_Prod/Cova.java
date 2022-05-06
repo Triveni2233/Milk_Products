d59d702bb41347086b789f3e07a36f002af4fd95
Charvik <Charvik@LAPTOP-S0IS0QVR>
2022-05-06 11:14:39 +0530

    Milk Execution

---
+++ src/Mil_Prod/Cova.java
@@ -0,0 +1,19 @@
+package Mil_Prod;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Cova {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
---
+++ src/Mil_Prod/package-info.java
@@ -0,0 +1,1 @@
+package Mil_Prod;
